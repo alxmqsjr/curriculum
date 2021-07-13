d079e405de79393d215ccdd46138b95b4fafb4bf
Alexandre Marques <alxmqsjr@gmail.com>
2021-07-13 20:19:06 -0300

    Alterações e adições

--- inc/color.php
+++ inc/color.php
@@ -2,86 +2,86 @@
 
 /*Para configurar as mudanças de cores da página*/
     
-    .bg-primary, .btn-primary {
-    	background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
-    } 
+.bg-primary, .btn-primary {
+    background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
+} 
 
-    .resume-section .text-primary, .resume-section .resume-section-link  {
-    	color: <?php echo get_theme_mod('cr_color'); ?> !important;
-    }
-    .resume-section .social-icons .social-icon:hover {
-        background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
-    }
+.resume-section .text-primary, .resume-section .resume-section-link  {
+    color: <?php echo get_theme_mod('cr_color'); ?> !important;
+}
+.resume-section .social-icons .social-icon:hover {
+    background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
+}
 
-    .btn-primary.disabled, .btn-primary:disabled, .btn-outline-primary:hover, {
-    	background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
-    }
+.btn-primary.disabled, .btn-primary:disabled, .btn-outline-primary:hover, {
+    background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
+}
 
-    .navbar-dark .navbar-nav .menu-item a,   .navbar-dark .navbar-nav .nav-link {
-  		color: <?php echo get_theme_mod('cr_color_menu'); ?> !important;
-	}
+.navbar-dark .navbar-nav .menu-item a,   .navbar-dark .navbar-nav .nav-link {
+    color: <?php echo get_theme_mod('cr_color_menu'); ?> !important;
+}
 
-    .navbar-dark .navbar-nav .menu-item a:hover, .navbar-dark .navbar-nav .menu-item a:focus,
-    .navbar-dark .navbar-nav .nav-link:hover, .navbar-dark .navbar-nav .nav-link:focus {
-        color: <?php { echo get_theme_mod('cr_color_menu_hover');?> !important;  <?php } ?>
-	}
+.navbar-dark .navbar-nav .menu-item a:hover, .navbar-dark .navbar-nav .menu-item a:focus,
+.navbar-dark .navbar-nav .nav-link:hover, .navbar-dark .navbar-nav .nav-link:focus {
+    color: <?php { echo get_theme_mod('cr_color_menu_hover');?> !important;  <?php } ?>
+}
 
-    .navbar-dark .navbar-nav .show > .menu-item a,     
-    .navbar-dark .navbar-nav .active > .menu-item a,
-    .navbar-dark .navbar-nav .menu-item a.show,
-    .navbar-dark .navbar-nav .menu-item a.active {
-        color: <?php { echo get_theme_mod('cr_color_menu_hover');?> !important;  <?php } ?>
-    }
+.navbar-dark .navbar-nav .show > .menu-item a,     
+.navbar-dark .navbar-nav .active > .menu-item a,
+.navbar-dark .navbar-nav .menu-item a.show,
+.navbar-dark .navbar-nav .menu-item a.active {
+    color: <?php { echo get_theme_mod('cr_color_menu_hover');?> !important;  <?php } ?>
+}
 
-    .navbar-dark .navbar-nav .show > .nav-link,
-    .navbar-dark .navbar-nav .active > .nav-link,
-    .navbar-dark .navbar-nav .nav-link.show,
-    .navbar-dark .navbar-nav .nav-link.active {
-        color: <?php { echo get_theme_mod('cr_color_menu_hover');?> !important;  <?php } ?>
-	}
+.navbar-dark .navbar-nav .show > .nav-link,
+.navbar-dark .navbar-nav .active > .nav-link,
+.navbar-dark .navbar-nav .nav-link.show,
+.navbar-dark .navbar-nav .nav-link.active {
+    color: <?php { echo get_theme_mod('cr_color_menu_hover');?> !important;  <?php } ?>
+}
 
-    .dropdown-item.active, .dropdown-item:active {
-    	background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
-    }
+.dropdown-item.active, .dropdown-item:active {
+    background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
+}
 
-    .custom-control-input:checked ~ .custom-control-label::before, 
-    .custom-checkbox .custom-control-input:indeterminate ~ .custom-control-label::before, 
-    .custom-range::-webkit-slider-thumb, .custom-range::-moz-range-thumb, .custom-range::-ms-thumb {
-    	background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
-    }
+.custom-control-input:checked ~ .custom-control-label::before, 
+.custom-checkbox .custom-control-input:indeterminate ~ .custom-control-label::before, 
+.custom-range::-webkit-slider-thumb, .custom-range::-moz-range-thumb, .custom-range::-ms-thumb {
+    background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
+}
 
-    .page-item.active .page-link, .badge-primary, .progress-bar, .list-group-item.active {
-        background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
-    }
+.page-item.active .page-link, .badge-primary, .progress-bar, .list-group-item.active {
+    background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
+}
 
-    .border-primary, .btn-primary, .btn-outline-primary, .btn-primary.disabled, .btn-primary:disabled, .btn-outline-primary:hover {
-    	border-color: <?php echo get_theme_mod('cr_color'); ?> !important;
-    } 
+.border-primary, .btn-primary, .btn-outline-primary, .btn-primary.disabled, .btn-primary:disabled, .btn-outline-primary:hover {
+    border-color: <?php echo get_theme_mod('cr_color'); ?> !important;
+} 
 
-    .custom-control-input:checked ~ .custom-control-label::before, .page-item.active .page-link, .list-group-item.active {
-    	border-color: <?php echo get_theme_mod('cr_color'); ?> !important;
-    }
+.custom-control-input:checked ~ .custom-control-label::before, .page-item.active .page-link, .list-group-item.active {
+    border-color: <?php echo get_theme_mod('cr_color'); ?> !important;
+}
 
-    .btn-outline-primary, .btn-link, .page-link {
-    	color: <?php echo get_theme_mod('cr_color'); ?> !important;
-    }
+.btn-outline-primary, .btn-link, .page-link {
+    color: <?php echo get_theme_mod('cr_color'); ?> !important;
+}
 
-    .btn-outline-primary:not(:disabled):not(.disabled):active, 
-    .btn-outline-primary:not(:disabled):not(.disabled).active, 
-    .show > .btn-outline-primary.dropdown-toggle {
-		background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
-        border-color: <?php echo get_theme_mod('cr_color'); ?> !important;
-    }
+.btn-outline-primary:not(:disabled):not(.disabled):active, 
+.btn-outline-primary:not(:disabled):not(.disabled).active, 
+.show > .btn-outline-primary.dropdown-toggle {
+    background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
+    border-color: <?php echo get_theme_mod('cr_color'); ?> !important;
+}
 
-    .nav-pills .menu-item a.active,
-    .nav-pills .show > .menu-item a,
-    .nav-pills .nav-link.active,
-    .nav-pills .show > .nav-link {
-		background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
-	}
-    
-    .text-warning {
-        color: <?php echo get_theme_mod('cr_color_award'); ?> !important;   
-    }
+.nav-pills .menu-item a.active,
+.nav-pills .show > .menu-item a,
+.nav-pills .nav-link.active,
+.nav-pills .show > .nav-link {
+    background-color: <?php echo get_theme_mod('cr_color'); ?> !important;
+}
+
+.text-warning {
+    color: <?php echo get_theme_mod('cr_color_award'); ?> !important;   
+}
     
 </style>
--- inc/customize-parts/layout.php
+++ inc/customize-parts/layout.php
@@ -1,53 +1,44 @@
 <?php
 
+// Fonte de texto
+$wp_customize->add_setting('cr_font', array('default'=>''));
+$wp_customize->add_control( 'cr_font', array(
+	'label' => __( 'Para modificar a fonte do texto, acesse "https://fonts.google.com", selecione a fonte desejada e seu estilo, copie o código do "@import" e cole-o no campo abaixo:', 'curriculum' ), 
+	'section' => 'cr_config_layout', 
+	'type' => 'textarea',
+	'input_attrs' => array ('placeholder' => __('ex: <style>@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap");</style>')	
+	)
+));
 
+// Nome da fonte
+$wp_customize->add_setting('cr_fontname', array('default'=>''));
+$wp_customize->add_control('cr_fontname', array(
+	'label' => __( 'Digite o nome completo e correto da fonte que você escolheu (respeitando espaços, letras maiúsculas e minúsculas):', 'curriculum' ), 
+	'section' => 'cr_config_layout', 
+	'type' => 'text',
+	'input_attrs' => array ('placeholder' => __('ex: Open Sans'), 'style' => 'margin-bottom:30px !important;')
+));
 
-	//Fonte de texto
-    $wp_customize->add_setting('cr_font', array('default'=>''));
-	$wp_customize->add_control( 'cr_font', array(
-			'label' => __( 'Para modificar a fonte do texto, acesse "https://fonts.google.com", selecione a fonte desejada e seu estilo, copie o código do "@import" e cole-o no campo abaixo:', 'curriculum' ), 
-			'section' => 'cr_config_layout', 
-			'type' => 'textarea',
-			'input_attrs' => array (
-				'placeholder' => __('ex: <style>@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap");</style>')	
-			)
-    ));
+// Cor primária
+$wp_customize->add_setting('cr_color', array('default'=>''));
+$wp_customize->add_control( new WP_Customize_Color_Control($wp_customize, 'cr_color', array(
+	'label' => __( 'Cor de destaque:', 'curriculum' ), 
+	'section' => 'cr_config_layout',
+	'settings' => 'cr_color'
+)));
 
-    //Nome da fonte
-    $wp_customize->add_setting('cr_fontname', array('default'=>''));
-	$wp_customize->add_control('cr_fontname', array(
-			'label' => __( 'Digite o nome completo e correto da fonte que você escolheu (respeitando espaços, letras maiúsculas e minúsculas):', 'curriculum' ), 
-			'section' => 'cr_config_layout', 
-			'type' => 'text',
-			'input_attrs' => array (
-				'placeholder' => __('ex: Open Sans'),
-				'style' => 'margin-bottom:30px !important;'
-			)
-    ));
+// Cor do menu
+$wp_customize->add_setting('cr_color_menu', array('default'=>''));
+$wp_customize->add_control( new WP_Customize_Color_Control($wp_customize, 'cr_color_menu', array(
+	'label' => __( 'Cor do menu:', 'curriculum' ), 
+	'section' => 'cr_config_layout',
+	'settings' => 'cr_color_menu'
+)));
 
-    //Cor primária
-  	$wp_customize->add_setting('cr_color', array('default'=>''));
-	$wp_customize->add_control( 
-		new WP_Customize_Color_Control($wp_customize, 'cr_color', array(
-			'label' => __( 'Cor de destaque:', 'curriculum' ), 
-			'section' => 'cr_config_layout',
-			'settings' => 'cr_color'
-    )));
-
-    //Cor do menu
-  	$wp_customize->add_setting('cr_color_menu', array('default'=>''));
-	$wp_customize->add_control( 
-		new WP_Customize_Color_Control($wp_customize, 'cr_color_menu', array(
-			'label' => __( 'Cor do menu:', 'curriculum' ), 
-			'section' => 'cr_config_layout',
-			'settings' => 'cr_color_menu'
-    )));
-
-    //Cor hover do menu
-  	$wp_customize->add_setting('cr_color_menu_hover', array('default'=>''));
-	$wp_customize->add_control( 
-		new WP_Customize_Color_Control($wp_customize, 'cr_color_menu_hover', array(
-			'label' => __( 'Cor hover do menu (quando passa o mouse):', 'curriculum' ), 
-			'section' => 'cr_config_layout',
-			'settings' => 'cr_color_menu_hover',
-    )));
+// Cor hover do menu
+$wp_customize->add_setting('cr_color_menu_hover', array('default'=>''));
+$wp_customize->add_control( new WP_Customize_Color_Control($wp_customize, 'cr_color_menu_hover', array(
+	'label' => __( 'Cor hover do menu (quando passa o mouse):', 'curriculum' ), 
+	'section' => 'cr_config_layout',
+	'settings' => 'cr_color_menu_hover',
+)));
