d83d11e451e28057c1e5f3091f5d798b565e3982
Alexandre Marques <alxmqsjr@gmail.com>
2021-07-13 20:10:36 -0300

    Alterações e adições

--- inc/customize-parts/interests.php
+++ inc/customize-parts/interests.php
@@ -1,35 +1,29 @@
 <?php 
 	
-	//Título da sessão
-    $wp_customize->add_setting('cr_interest_title', array('default'=>''));
-	$wp_customize->add_control('cr_interest_title', array(
-			'label' => __( 'Título da Sessão:', 'curriculum' ), 
-			'section' => 'cr_config_interests', 
-			'type' => 'text',
-			'input_attrs' => array (
-				'style' => 'margin-bottom:20px'
-			)
-    ));
+// Título da sessão
+$wp_customize->add_setting('cr_interest_title', array('default'=>''));
+$wp_customize->add_control('cr_interest_title', array(
+	'label' => __( 'Título da Sessão:', 'curriculum' ), 
+	'section' => 'cr_config_interests', 
+	'type' => 'text',
+	'input_attrs' => array ('style' => 'margin-bottom:20px')
+));
 
-	//Descrição dos interesses
-    $wp_customize->add_setting('cr_interest_desc_1', array('default'=>''));
-	$wp_customize->add_control('cr_interest_desc_1', array(
-			'label' => __( 'Descreva sobre você, suas metas, personalidade, etc. (este campo equivale a um parágrafo):', 'curriculum' ), 
-			'section' => 'cr_config_interests', 
-			'type' => 'textarea',
-			'input_attrs' => array (
-				'style' => 'height:150px'
-			)
-    ));
+// Descrição dos interesses
+$wp_customize->add_setting('cr_interest_desc_1', array('default'=>''));
+$wp_customize->add_control('cr_interest_desc_1', array(
+	'label' => __( 'Descreva sobre você, suas metas, personalidade, etc. (este campo equivale a um parágrafo):', 'curriculum' ), 
+	'section' => 'cr_config_interests', 
+	'type' => 'textarea',
+	'input_attrs' => array ('style' => 'height:150px')
+));
 
-	//Descrição dos interesses
-    $wp_customize->add_setting('cr_interest_desc_2', array('default'=>''));
-	$wp_customize->add_control('cr_interest_desc_2', array(
-			'label' => __( 'Quer adicionar mais informações para o segundo parágrafo?', 'curriculum' ), 
-			'section' => 'cr_config_interests', 
-			'type' => 'textarea',
-			'input_attrs' => array (
-				'style' => 'height:150px'
-			)
-    ));
+// Descrição dos interesses
+$wp_customize->add_setting('cr_interest_desc_2', array('default'=>''));
+$wp_customize->add_control('cr_interest_desc_2', array(
+	'label' => __( 'Quer adicionar mais informações para o segundo parágrafo?', 'curriculum' ), 
+	'section' => 'cr_config_interests', 
+	'type' => 'textarea',
+	'input_attrs' => array ('style' => 'height:150px')
+));
 
